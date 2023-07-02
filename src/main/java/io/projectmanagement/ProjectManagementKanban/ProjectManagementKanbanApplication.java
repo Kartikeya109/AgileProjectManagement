3e49430da15e918bac749fd23bc997dc2c0885cb
Kartik <elliter075@gmail.com>
2023-07-02 13:29:11 +0530

    first setup spring commit

---
+++ src/main/java/io/projectmanagement/ProjectManagementKanban/ProjectManagementKanbanApplication.java
@@ -0,0 +1,13 @@
+package io.projectmanagement.ProjectManagementKanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementKanbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementKanbanApplication.class, args);
+	}
+
+}
